68cf32f52cb91ada82181efa29d55da7084ebd3e
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2024-01-21 04:01:28 +0900

    test

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    // baseURL: "http://43.201.122.183", 
+    baseURL: "http://43.201.122.183", 
     headers: {
         "Content-Type": "application/json"
     }
